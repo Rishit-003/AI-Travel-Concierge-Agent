6074c698bb522fd0cbbe889b448bb434fb272174
Rishit <ambaliyarishit3635@gmail.com>
2026-03-09 23:10:09 +0530

    editing tools.py

--- backend/agent/tools.py
+++ backend/agent/tools.py
@@ -1,5 +1,5 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
-from backend.rag.config import GEMINI_API_KEY
+from backend.config import GEMINI_API_KEY
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
